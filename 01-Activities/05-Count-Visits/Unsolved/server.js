4017c04442b2edc02a4db8b8e479b9041325cf52
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-21 11:32:58 -0500

    activity 5

--- 01-Activities/05-Count-Visits/Unsolved/server.js
+++ 01-Activities/05-Count-Visits/Unsolved/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 // Require the 'express-session' module
 // YOUR CODE HERE
-//
+const session = require('express-session');
 
 // Sets up the Express App
 // =============================================================
@@ -18,7 +18,13 @@ app.set('view engine', 'handlebars');
 // Set up the session with the 'secret', 'resave', 'saveUninitialized' options
 //
 // YOUR CODE HERE
-//
+app.use(
+  session ({
+    secret: "Super secret secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Data
 // =============================================================
@@ -61,21 +67,23 @@ const books = [
 app.get('/', (req, res) => {
   if (req.session.countVisit) {
     // If the 'countVisit' session variable exists, increment it by 1 and set the 'firstTime' session variable to 'false'
-    //
     // YOUR CODE HERE
-    //
+    console.log('not first time')
+    req.session.countVisit++
+    req.session.firstTime = false;
   } else {
     // If the 'countVisit' session variable doesn't exist, set it to 1 and set the 'firstTime' session variable to 'true'
-    //
     // YOUR CODE HERE
-    //
+    console.log('first time')
+    req.session.countVisit = 1;
+    req.session.firstTime = true;
   }
-
   const data = {
     // Include the 'books' array, 'countVisit' and 'firstTime' session variables to be sent over to `index.handlebars`
-    //
     // YOUR CODE HERE
-    //
+    library: books,
+    //req.session.countVisit
+    
   };
   res.render('index', data);
 });
